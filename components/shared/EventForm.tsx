9df5cdc4d130b0858341726f1e47bee7a46c0dfc
EpicSWAGCoding <EpicSWAG123@yandex.com>
2024-01-25 13:53:49 +0300

    Form Submission & Actions

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,86 +2,149 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import * as z from "zod"
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { eventFormSchema } from "@/lib/validator"
+import * as z from 'zod'
 import { eventDefaultValues } from "@/constants"
 import Dropdown from "./Dropdown"
 import { Textarea } from "@/components/ui/textarea"
+import { FileUploader } from "./FileUploader"
 import { useState } from "react"
 import Image from "next/image"
 import DatePicker from "react-datepicker";
+import { useUploadThing } from '@/lib/uploadthing'
+
+import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "../ui/checkbox"
-import { FileUploader } from "./FileUploader"
+import { useRouter } from "next/navigation"
+import { createEvent, updateEvent } from "@/lib/actions/event.actions"
+import { IEvent } from "@/lib/database/models/event.model"
+
 
 type EventFormProps = {
-    userId: string
-    type: "Create" | "Update"
-  }
+  userId: string
+  type: "Create" | "Update"
+  event?: IEvent,
+  eventId?: string
+}
 
-const EventForm = ({ userId, type }: EventFormProps) => {
-
-    const [files, setFiles] = useState<File[]>([])
-
-    const initialValues = eventDefaultValues;
-
-    const form = useForm<z.infer<typeof eventFormSchema>>({
-        resolver: zodResolver(eventFormSchema),
-        defaultValues: initialValues
-    })
-
-    function onSumbit(values: z.infer<typeof eventFormSchema>) {
-        console.log(values)
+const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
+  const [files, setFiles] = useState<File[]>([])
+  const initialValues = event && type === 'Update' 
+    ? { 
+      ...event, 
+      startDateTime: new Date(event.startDateTime), 
+      endDateTime: new Date(event.endDateTime) 
     }
+    : eventDefaultValues;
+  const router = useRouter();
+
+  const { startUpload } = useUploadThing('imageUploader')
+
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues
+  })
+ 
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    let uploadedImageUrl = values.imageUrl;
+
+    if(files.length > 0) {
+      const uploadedImages = await startUpload(files)
+
+      if(!uploadedImages) {
+        return
+      }
+
+      uploadedImageUrl = uploadedImages[0].url
+    }
+
+    if(type === 'Create') {
+      try {
+        const newEvent = await createEvent({
+          event: { ...values, imageUrl: uploadedImageUrl },
+          userId,
+          path: '/profile'
+        })
+
+        if(newEvent) {
+          form.reset();
+          router.push(`/events/${newEvent._id}`)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if(type === 'Update') {
+      if(!eventId) {
+        router.back()
+        return;
+      }
+
+      try {
+        const updatedEvent = await updateEvent({
+          userId,
+          event: { ...values, imageUrl: uploadedImageUrl, _id: eventId },
+          path: `/events/${eventId}`
+        })
+
+        if(updatedEvent) {
+          form.reset();
+          router.push(`/events/${updatedEvent._id}`)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
   return (
     <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSumbit)} className="flex flex-col gap-5">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Event title" {...field} className="input-field" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
 
-            <div className="flex flex-col gap-5 md:flex-row">
-                <FormField
-                    control={form.control} 
-                    name="title"
-                    render={({ field }) => (
-                        <FormItem className="w-full">
-                            <FormControl>
-                                <Input placeholder="Event title" {...field} className="input-field" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control} 
-                    name="categoryId"
-                    render={({ field }) => (
-                        <FormItem className="w-full">
-                            <FormControl>
-                                <Dropdown onChangeHandler={field.onChange} value={field.value} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-            </div>
-
-            <div className="flex flex-col gap-5 md:flex-row">
-                <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                    <FormItem className="w-full">
-                    <FormControl className="h-72">
-                        <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                    )}
-                />
-            
-                <FormField
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl className="h-72">
+                    <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          <FormField
               control={form.control}
               name="imageUrl"
               render={({ field }) => (
@@ -97,9 +160,9 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                 </FormItem>
               )}
             />
-            </div>
+        </div>
 
-            <div className="flex flex-col gap-5 md:flex-row">
+        <div className="flex flex-col gap-5 md:flex-row">
           <FormField
               control={form.control}
               name="location"
@@ -254,8 +317,17 @@ const EventForm = ({ userId, type }: EventFormProps) => {
             />
         </div>
 
-            <Button type="submit">Sumbit</Button>
-        </form>
+
+        <Button 
+          type="submit"
+          size="lg"
+          disabled={form.formState.isSubmitting}
+          className="button col-span-2 w-full"
+        >
+          {form.formState.isSubmitting ? (
+            'Submitting...'
+          ): `${type} Event `}</Button>
+      </form>
     </Form>
   )
 }
