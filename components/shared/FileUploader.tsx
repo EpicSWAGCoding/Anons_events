49cefcbafb6a521774366f1615e847422652d59c
EpicSWAGCoding <EpicSWAG123@yandex.com>
2024-01-25 13:00:39 +0300

    fixed FileUploader

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,57 +1,53 @@
-// import React, { Dispatch, SetStateAction } from 'react';
-// import { UploadDropzone } from "@uploadthing/react";
-// import type { FileWithPath } from '@uploadthing/react';
-// import { generateClientDropzoneAccept } from 'uploadthing/client';
+'use client'
 
-// type FileUploaderProps = {
-//   imageUrl: string;
-//   onFieldChange: (url: string) => void;
-//   setFiles: Dispatch<SetStateAction<File[]>>;
-// };
+import { useCallback, Dispatch, SetStateAction } from 'react'
+import { useDropzone } from '@uploadthing/react/hooks'
+import { generateClientDropzoneAccept } from 'uploadthing/client'
 
-// const FileUploader: React.FC<FileUploaderProps> = ({ imageUrl, onFieldChange, setFiles }) => {
-//   const onDrop = (acceptedFiles: FileWithPath[]) => {
-//     setFiles(acceptedFiles);
-//     onFieldChange(convertFileToUrl(acceptedFiles[0]));
-//   };
+import { Button } from '@/components/ui/button'
 
-//   return (
-//     <UploadDropzone
-//       onDrop={onDrop}
-//       accept={generateClientDropzoneAccept(['image/*'])}
-//       onClientUploadComplete={(res) => {
-//         console.log("Files: ", res);
-//         alert("Upload Completed");
-//       }}
-//       onUploadError={(error: Error) => {
-//         alert(`ERROR! ${error.message}`);
-//       }}
-//       onUploadBegin={(name) => {
-//         console.log("Uploading: ", name);
-//       }}
-//     >
-//       {imageUrl ? (
-//         <div className="flex h-full w-full flex-1 justify-center">
-//           <img
-//             src={imageUrl}
-//             alt="image"
-//             width={250}
-//             height={250}
-//             className="w-full object-cover object-center"
-//           />
-//         </div>
-//       ) : (
-//         <div className="flex-center flex-col py-5 text-grey-500">
-//           <img src="/assets/icons/upload.svg" width={77} height={77} alt="file upload" />
-//           <h3 className="mb-2 mt-2">Drag photo here</h3>
-//           <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
-//           <Button type="button" className="rounded-full">
-//             Select from computer
-//           </Button>
-//         </div>
-//       )}
-//     </UploadDropzone>
-//   );
-// };
+type FileUploaderProps = {
+  onFieldChange: (url: string) => void
+  imageUrl: string
+  setFiles: Dispatch<SetStateAction<File[]>>
+}
 
-// export default FileUploader;
+export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploaderProps) {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'image/*' ? generateClientDropzoneAccept(['image/*']) : undefined,
+  })
+
+  return (
+    <div
+      {...getRootProps()}
+      className="flex-center bg-dark-3 flex h-72 cursor-pointer flex-col overflow-hidden rounded-xl bg-grey-50">
+      <input {...getInputProps()} className="cursor-pointer" />
+
+      {imageUrl ? (
+        <div className="flex h-full w-full flex-1 justify-center ">
+          <img
+            src={imageUrl}
+            alt="image"
+            width={250}
+            height={250}
+            className="w-full object-cover object-center"
+          />
+        </div>
+      ) : (
+        <div className="flex-center flex-col py-5 text-grey-500">
+          <img src="/assets/icons/upload.svg" width={77} height={77} alt="file upload" />
+          <h3 className="mb-2 mt-2">Drag photo here</h3>
+          <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
+          <Button type="button" className="rounded-full">
+            Select from computer
+          </Button>
+        </div>
+      )}
+    </div>
+  )
+}
